05381ccf2bf7eb3d9c29f1090d9fa49fa7b62261
Yao <capslock0921@gmail.com>
2020-07-25 22:55:44 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -1,19 +1,28 @@
-// require packages used in the project
 const express = require('express')
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
 const app = express()
 const port = 3000
 const exphbs = require('express-handlebars')
 const restaurantList = require('./restaurants.json')
-const fontawesome = require('@fortawesome/fontawesome-free')
+const Restaurant = require('./models/restaurant')
 
-// setting template engine
+mongoose.connect('mongodb://localhost/restaurant-list', { useNewUrlParser: true, useUnifiedTopology: true })
+const db = mongoose.connection
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
+
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
-// setting static files
+app.use(bodyParser.urlencoded({ extended: true }))
+
 app.use(express.static('public'))
 
-// routes setting
 app.get('/', (req, res) => {
   res.render('index', { restaurants: restaurantList.results })
 })
@@ -26,13 +35,15 @@ app.get('/search', (req, res) => {
   res.render('index', { restaurants: restaurants, keyword: keyword })
 })
 
+app.get('/restaurants/new', (req, res) => {
+  return res.render('new')
+})
+
 app.get('/restaurants/:restaurant_id', (req, res) => {
-  // console.log(req.params.restaurant_id)
   const restaurant = restaurantList.results.find(restaurant => restaurant.id.toString() === req.params.restaurant_id)
   res.render('show', { restaurant: restaurant })
 })
 
-// start and listen on the Express server
 app.listen(port, () => {
   console.log(`Express is listening on localhost:${port}`)
 })
