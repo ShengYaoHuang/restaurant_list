896a3a540ebe84e310b960724926ad4edb2184ab
Yao <capslock0921@gmail.com>
2020-07-28 22:01:39 +0800

    refactor: modify express routes

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
+const routes = require('./routes')
 
 const restaurantList = require('./models/seeds/restaurants.json')
 const Restaurant = require('./models/restaurant')
@@ -24,71 +25,7 @@ app.set('view engine', 'handlebars')
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.static('public'))
 app.use(methodOverride('_method'))
-
-app.get('/', (req, res) => {
-  Restaurant.find()
-    .lean()
-    // .sort({ _id: 'asc' })
-    .sort({ name: 'asc' })
-    .then(restaurants => res.render('index', { restaurants }))
-    .catch(error => console.error(error))
-})
-
-
-
-app.get('/restaurants/new', (req, res) => {
-  return res.render('new')
-})
-
-app.post('/restaurants', (req, res) => {
-  const restaurant = req.body
-  return Restaurant.create(restaurant)
-    .then(() => res.redirect('/'))
-    .catch(error => console.log(error))
-})
-
-app.get('/restaurants/:id', (req, res) => {
-  const id = req.params.id
-  return Restaurant.findById(id)
-    .lean()
-    .then((restaurant) => res.render('show', { restaurant }))
-    .catch(error => console.log(error))
-})
-
-app.get('/restaurants/:id/edit', (req, res) => {
-  const id = req.params.id
-  return Restaurant.findById(id)
-    .lean()
-    .then((restaurant) => res.render('edit', { restaurant }))
-    .catch(error => console.log(error))
-})
-
-app.put('/restaurants/:id', (req, res) => {
-  const id = req.params.id
-  return Restaurant.findById(id)
-    .then(restaurant => {
-      restaurant = Object.assign(restaurant, req.body)
-      return restaurant.save()
-    })
-    .then(() => res.redirect(`/restaurants/${id}`))
-    .catch(error => console.log(error))
-})
-
-app.delete('/restaurants/:id', (req, res) => {
-  const id = req.params.id
-  return Restaurant.findById(id)
-    .then(restaurant => restaurant.remove())
-    .then(() => res.redirect('/'))
-    .catch(error => console.log(error))
-})
-
-app.get('/search', (req, res) => {
-  const keyword = req.query.keyword
-  const restaurants = restaurantList.results.filter(restaurant => {
-    return restaurant.name.toLocaleLowerCase().includes(keyword.toLowerCase())
-  })
-  res.render('index', { restaurants, keyword })
-})
+app.use(routes)
 
 app.listen(3000, () => {
   console.log(`Express is listening on localhost:3000`)
---
+++ routes/modules/home.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const router = express.Router()
+
+const Restaurant = require('../../models/restaurant')
+
+router.get('/', (req, res) => {
+  Restaurant.find()
+    .lean()
+    // .sort({ _id: 'asc' })
+    .sort({ name: 'asc' })
+    .then(restaurants => res.render('index', { restaurants }))
+    .catch(error => console.error(error))
+})
+
+router.get('/search', (req, res) => {
+  const keyword = req.query.keyword
+  Restaurant.find({ name: { $regex: keyword, $options: "i" } })
+    .lean()
+    .then(restaurants => res.render('index', { restaurants }))
+    .catch(error => console.log(error))
+})
+module.exports = router
